17397fa5eb028db2d86276a8d5c72a4a9067852d
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-04-19 20:55:16 +0200

    Implementing parsing & Changing main skeleton to support stages (parsing, solving, visualizing)

--- main.cpp
+++ main.cpp
@@ -1,69 +1,131 @@
+#include <cmip.h>
 #include <except.h>
 #include <iostream>
-int variant1()
-{
-        try {
-                CMIP prob("MIPCLtest"); // 1
-                prob.openMatrix(2,2,4); // 2
-                prob.addVar(0,CMIP::VAR_INT, 100.0,0.0, CLP::VAR_INF); // 3
-                prob.addVar(1,CMIP::VAR_INT,  64.0,0.0, CLP::VAR_INF); // 4
-                prob.addCtr(0,0,-CLP::INF,250); // 5
-                prob.addCtr(1,0,-CLP::INF,4); // 6
-                prob.addEntry(50.0,0,0); // 7.1
-                prob.addEntry(31.0,0,1); // 7.2
-                prob.addEntry(-3.0,1,0); // 7.3
-                prob.addEntry( 2.0,1,1); // 7.4
-                prob.closeMatrix(); // (8)
-                prob.optimize(); // (9)
-                prob.printSolution("primer.sol"); // (10)
-        }
-        catch(CException* pe) {
-                std::cerr << pe->what() << std::endl;
-                delete pe;
-                return 1;
-        }
-        return 0;
-} // end of variant1
+#include <vector>
+#include <string>
 
+using namespace std;
 
-
-// Constants that represent our test IP.
-const int n=2, m=2, nz=4;
-double c[] = {100,64}; // cost vector
-double A[][n] = { // matrix
-                {50,31},
-                {-3,2}
+struct HardReactangle{
+        int length;
+        int width;
+        int inputOrder;
+        int count;
+        long x_position;
+        long y_position;
 };
-double b[] = {250,4}; // right-hand side vector
-int ind[] = {0,1}; // array of indices
-int variant2()
-{
+
+
+struct SoftRectangle{
+        int area;
+        int predicted_length;
+        int predicted_width;
+        long x_position;
+        long y_position;
+};
+
+bool parseForVector(vector<HardReactangle>& Rects, ifstream& inputFile){
+        int count = 0;
+        inputFile >> count;
+        Rects.resize(count);
+
+        int offset = 0;
+        for (int i =0; i < count; i++){
+                inputFile >> dieHarRectsdRectangles[i + offset].length >> Rects[i + offset].width >> Rects[i + offset].count;
+                Rects[i + offset].inputOrder = i;
+
+                if(Rects[i + offset].count > 1){
+                        Rects.resize(Rects.size() + Rects[i + offset].count - 1);
+                        for (int j = 1; j <  Rects[i + offset].count; j++){
+                                Rects[i + offset + j].length = Rects[i + offset].length;
+                                Rects[i + offset + j].width = Rects[i + offset].width;
+                                Rects[i + offset + j].count = Rects[i + offset].count;
+                                Rects[i + offset + j].inputOrder = Rects[i + offset].inputOrder;                                        
+                        }
+
+                        offset += (Rects[i + offset].count  - 1);
+                }
+
+        }
+
+        return true;
+}
+
+
+bool parseInputConstraints(vector<HardReactangle>& dieRectangles, vector<HardReactangle>& coreRectangles, vector<SoftRectangle>& softRectangles, string& fileName){
+        ifstream inputFile (fileName);
+        
+        if (inputFile.is_open()){
+                //reading first rectangles type
+                parseForVector(dieRectangles, inputFile);
+
+                //reading second rectangles type
+                parseForVector(coreRectangles, inputFile);
+
+                //reading third rectangles type
+                int softCount = 0;
+                inputFile >> softCount;
+                for (int i =0; i < softCount; i++){
+                        inputFile >> softRectangles[i].area;
+                }
+
+
+                inputFile.close();
+        }else{
+                cout << "Error Opening Specified file";
+                return false;
+        }
+
+        return true;
+}
+
+
+
+bool solveProblem(){
         try {
-                CMIP prob("MIPCLtest"); // create new MIP problem
-                prob.openMatrix(n,m,nz); // open matrix
-                for (int j=0; j < n; ++j) // adds n variables:
-                        prob.addVar(j,CMIP::VAR_INT, c[j],0.0, CLP::VAR_INF);
-                for (int i=0; i < m; ++i) // add m rows (constraints):
-                        prob.addRow(i,0,-CLP::INF,b[i],n,A[i],ind);
-                prob.closeMatrix(); // close matrix
-                prob.optimize(); // solve problem
-                prob.printSolution("primer.sol"); // print solution
+		CMIP prob("MIPCLtest"); // 1
+		prob.openMatrix(2,2,4); // 2
+		prob.addVar(0,CMIP::VAR_INT, 100.0,0.0, CLP::VAR_INF); // 3
+		prob.addVar(1,CMIP::VAR_INT,  64.0,0.0, CLP::VAR_INF); // 4
+		prob.addCtr(0,0,-CLP::INF,250); // 5
+		prob.addCtr(1,0,-CLP::INF,4); // 6
+		prob.addEntry(50.0,0,0); // 7.1
+		prob.addEntry(31.0,0,1); // 7.2
+		prob.addEntry(-3.0,1,0); // 7.3
+		prob.addEntry( 2.0,1,1); // 7.4
+		prob.closeMatrix(); // (8)
+		prob.optimize(); // (9)
+		prob.printSolution("primer.sol"); // (10)
+	}
+	catch(CException* pe) {
+		std::cerr << pe->what() << std::endl;
+		delete pe;
+		return false;
+	}
+	return true;
+}
+
+
+
+bool visualizeProblem(){
+        return true;
+}
+
+int main(int argc, char *argv[]){
+        vector<HardReactangle> dieHardRectangles;
+        vector<HardReactangle> coreHardRectangles;
+        vector<SoftRectangle> softRectangles;
+
+        string fileName = argv[1];
+
+        if(!parseInputConstraints(dieHardRectangles, coreHardRectangles, softRectangles, fileName)){
+                cout << "Input File doesnot meet the specified requirements\n";
+                exit(-1);
         }
-        catch(CException* pe) {
-                std::cerr << pe->what() << std::endl;
-                delete pe;
-                return 1;
-        }
+
+        solveProblem(dieHardRectangles, coreHardRectangles, softRectangles);
+
+        
+
         return 0;
-}  // end of variant2
-
-
-
-int main(int argc, char *argv[])
-{
-        int v=1;
-        if (argc > 1)
-                if (argv[1][0] == '2')
-                        v=2;
-        return (v == 1)? variant1(): variant2();
 }
