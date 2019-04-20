e70a22aeb0949f9d3ca74048fac0d4af4f98b2e5
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-04-20 16:23:10 +0200

    Solving previous runs accumulation issue w default input paramters

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
+rm *.sol main.o app *.txt
+
 g++ -c -Imipcl-2.1.3/mipcl/headers -m64 -std=c++11 -DMIP_API= main.cpp
 g++ main.o -Lmipcl-2.1.3/lib -lmipcl  -o app -std=c++11
 
-rm *.sol main.o app 
 
-./app 2
+./app input.in 50 100
