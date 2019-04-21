778d8af4cf0a857c5e82ecbd7f8d96de51a6b33f
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-04-21 09:36:05 +0200

    12 General  Test Cases Added

--- test.sh
+++ test.sh
@@ -1,7 +1,21 @@
-rm *.sol main.o app *.txt
+rm *.sol main.o app *.txt ./Out/*
 
 g++ -c -Imipcl-2.1.3/mipcl/headers -m64 -std=c++11 -DMIP_API= main.cpp
 g++ main.o -Lmipcl-2.1.3/lib -lmipcl  -o app -std=c++11
 
 
-./app input.in 50 100
+./app input.txt 10000 10000
+
+./app 01padsTestOne.txt 10000 10000
+./app 02padsTestTwo.txt 10000 10000
+./app 03padsTestThree.txt 10000 10000
+./app 04HardCoreCellsOne.txt 10000 10000
+./app 05HardCoreCellsTwo.txt 10000 10000
+./app 06HardCoreCellsThree.txt 10000 10000
+./app 07PadsAndCoreCellsOne.txt 10000 10000
+./app 08PadsAndCoreCellsTwo.txt 10000 10000
+./app 09SoftCellsOne.txt 10000 10000
+./app 10SoftCellsTwo.txt 10000 10000
+./app 11AllCellsOne.txt 10000 10000
+./app 12AllCellsTwo.txt 10000 10000
+
